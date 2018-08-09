bf5366423cd92ef42a5a0b9abd615ec7aea1dbf8
cristianrojas-dev <cristianrojas.dev@gmail.com>
2018-08-09 23:09:19 +1000

    Modified server/index.js
    -Require react
    -Require react-dom
    -Require component/home
    -Render to string component Home
    
    	modified:   server/src/index.js

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const React = require('react');
+const renderToString = require('react-dom/server').renderToString;
+const Home = require('./client/components/Home').default;
 const app = express();
-const renderToString = require('react-dom/server')
 
-app.get('/', (req , res) => {});
+app.get('/', (req , res) => {
+  const content = renderToString(<Home />);
+
+  res.send(content);
+});
 
 app.listen(3000, () => {
   console.log('Listen on port 3000')
