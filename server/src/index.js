2f9a64e64acd40d3536de78a92ca093caa5be680
cristianrojas-dev <cristianrojas.dev@gmail.com>
2018-08-12 05:49:23 +1000

    fix server/index.js file
    	modified:   src/index.js

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import React from 'react';
-import { renderToString } from 'reac-dom/server';
+import { renderToString } from 'react-dom/server';
 import Home from './client/components/Home';
 
 const app = express();
