37b8ae33f1a142e76f781a8bad346dfe71e41f2e
cristianrojas-dev <cristianrojas.dev@gmail.com>
2018-08-18 17:45:04 +1000

    Refactoring webpack
    -Add config webpack.base.js
    -Delete module-rules webpack.client
    -Delete module-rules webpack.server
    
    	copied:     webpack.client.js -> webpack.base.js
    	modified:   webpack.client.js
    	modified:   webpack.server.js

--- server/webpack.server.js
+++ server/webpack.server.js
@@ -1,27 +1,14 @@
-const path = require("path");
+const path = require('path');
+const merge = require ('webpack-merge');
+const baseConfig = require('./webpack.base.js');
 
-module.exports = {
+const config = {
   target: 'node',
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'build')
-
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        options: {
-          presets: [
-            'react',
-            'stage-0',
-            ['env', { targets: { browsers: ['last 2 versions'] } }]
-          ]
-        }
-      }
-    ]
   }
 };
+
+module.exports = merge(baseConfig, config);
