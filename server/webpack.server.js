5dae1ee791d22d9660086ea9d4bbd8ea3274ee11
cristianrojas-dev <cristianrojas.dev@gmail.com>
2018-08-18 20:57:00 +1000

    Fixed webpack.server
    -Add , in line 12
    
    	modified:   webpack.server.js

--- server/webpack.server.js
+++ server/webpack.server.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const merge = require ('webpack-merge');
 const baseConfig = require('./webpack.base.js');
+const webpackNodeExternals = require('webpack-node-externals')
 
 const config = {
   target: 'node',
@@ -8,7 +9,9 @@ const config = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'build')
-  }
+  },
+
+  externals: [webpackNodeExternals()]
 };
 
 module.exports = merge(baseConfig, config);
