4e2bd9d4c3336893ac5459df11f588858b2f8271
cristianrojas-dev <cristianrojas.dev@gmail.com>
2018-08-18 17:09:11 +1000

    Modified client.js
    -Change ReactDom.render() by ReactDom.hydrate
    
    	modified:   src/client/client.js

--- server/src/client/client.js
+++ server/src/client/client.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import Home from './components/Home';
 
-ReactDom.render(<Home />, document.querySelector('#root'));
+ReactDom.hydrate(<Home />, document.querySelector('#root'));
